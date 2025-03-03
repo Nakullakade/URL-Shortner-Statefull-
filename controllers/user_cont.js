18954099ebac7e07fb28b0714b601cc662180ced
Nakul Lakade <133630039+Nakullakade@users.noreply.github.com>
2025-03-03 17:24:16 +0530

    Add files via upload

---
+++ controllers/user_cont.js
@@ -0,0 +1,43 @@
+const User=require("../models/user.js");
+
+//to create a cookie value uuid is used
+const {v4:uuidv4}=require('uuid');
+
+//to set user with its cookie value(id) by using id
+const {setUser}=require("../service/auth.js");
+
+
+async function handleUserSignUp(req,res){
+const {name,email,password}=req.body;
+await User.create({
+    name,email,password
+});
+return res.redirect("/");
+//return res.render("home");
+}
+
+async function handleUserlogin(req,res){
+    const {email,password}=req.body;
+    const user=await User.findOne({
+        email,password
+    });
+    //console.log(user);
+    if(!user) 
+    return res.render("login",{
+    error:"Invalid Username or Password",
+});
+
+//register uuid to const 
+const sessionId=uuidv4();
+
+//set user with uuid
+setUser(sessionId,user);
+
+//create cookie with uid value
+res.cookie("uid",sessionId);
+    return res.redirect("/");
+}
+module.exports={
+    handleUserSignUp,
+    handleUserlogin,
+}
